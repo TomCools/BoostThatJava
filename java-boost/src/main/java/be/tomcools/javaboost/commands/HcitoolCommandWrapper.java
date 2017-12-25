05018eb60f5a7b5100e871bb6f6448e00baad8a1
tomco <tom.cools@infosupport.com>
2017-12-25 14:38:13 +0100

    Made it async

--- java-boost/src/main/java/be/tomcools/javaboost/commands/HcitoolCommandWrapper.java
+++ java-boost/src/main/java/be/tomcools/javaboost/commands/HcitoolCommandWrapper.java
@@ -5,6 +5,8 @@ import be.tomcools.javaboost.models.Device;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 public class HcitoolCommandWrapper implements BluetoothActions {
@@ -17,27 +19,37 @@ public class HcitoolCommandWrapper implements BluetoothActions {
 
     private String scanForDevices(int scanTimeInSeconds) {
         System.out.println("Scanning for devices...");
-        ProcessBuilder ps=new ProcessBuilder("sudo", "hcitool","lescan");
+        ProcessBuilder ps = new ProcessBuilder("sudo", "hcitool", "lescan");
         ps.redirectErrorStream(true);
-
         StringBuilder sb = new StringBuilder();
-        try {
-            Process pr = ps.start();
-            BufferedReader in = new BufferedReader(new InputStreamReader(pr.getInputStream()));
-            String line;
-            while ((line = in.readLine()) != null) {
-                System.out.println("Read line: " + line);
-                sb.append(line).append(System.lineSeparator());
-            }
-            System.out.println("Waiting for process.");
-            pr.waitFor(scanTimeInSeconds, TimeUnit.SECONDS);
-            pr.destroy();
-            System.out.println("ok!");
+        Future<?> future = Executors.newSingleThreadExecutor().submit(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Process pr = ps.start();
+                    BufferedReader in = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+                    String line;
+                    while ((line = in.readLine()) != null) {
+                        System.out.println("Read line: " + line);
+                        sb.append(line).append(System.lineSeparator());
+                    }
+                    System.out.println("Waiting for process.");
+                    pr.waitFor(scanTimeInSeconds, TimeUnit.SECONDS);
+                    pr.destroy();
+                    System.out.println("ok!");
 
-            in.close();
-        } catch (Exception ex) {
-            throw new IllegalStateException(ex);
+                    in.close();
+                } catch (Exception ex) {
+                    throw new IllegalStateException(ex);
+                }
+            }
+        });
+        try {
+            future.wait(3000);
+        } catch (InterruptedException e) {
+            throw new IllegalStateException(e);
         }
+
         return sb.toString();
     }
 }
